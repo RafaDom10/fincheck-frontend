99a1fda5b3d4be49e22337a41c669be9ac205a9a
Rafael Domingues <r-domingues@outlook.com>
2024-01-07 21:08:41 -0300

    refactor: implement register form interface

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,5 +1,51 @@
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
 export function Register () {
     return (
-        <h1>Register Page</h1>
+        <>
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <Link
+            to="/login"
+            className="tracking-[-0.5px] text-teal-900 font-medium"
+          >
+            Fazer login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          type="name"
+          placeholder="Nome"
+          name="name"
+        />
+
+        <Input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+        />
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          name="password"
+        />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
     )
 }
