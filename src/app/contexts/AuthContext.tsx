2828418009e853ec3b353574c5bed75bd0042c55
Rafael Domingues <r-domingues@outlook.com>
2024-01-11 21:12:52 -0300

    feat: add signin and signout methods

--- src/app/contexts/AuthContext.tsx
+++ src/app/contexts/AuthContext.tsx
@@ -1,14 +1,33 @@
-import { createContext } from "react";
+import { createContext, useCallback, useState } from "react";
+import { localStorageKeys } from "../config/localStorageKeys";
 
 interface AuthContextValue {
   signedIn: boolean
+  signin( accessToken: string ): void
+  signout(): void
 }
 
 export const AuthContext = createContext<AuthContextValue>({} as AuthContextValue)
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  return(
-    <AuthContext.Provider value={{ signedIn: true }}>
+  const [ signedIn, setSignedIn ] = useState<boolean>(() => {
+    const storageAccessToke = localStorage.getItem(localStorageKeys.ACCESS_TOKEN)
+
+    return !!storageAccessToke
+  })
+
+  const signin = useCallback(( accessToken: string ) => {
+    localStorage.setItem( localStorageKeys.ACCESS_TOKEN, accessToken )
+    setSignedIn(true)
+  }, [])
+
+  const signout = useCallback(() => {
+    localStorage.removeItem( localStorageKeys.ACCESS_TOKEN )
+    setSignedIn(false)
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ signedIn: signedIn, signin, signout }}>
       {children}
     </AuthContext.Provider>
   )
