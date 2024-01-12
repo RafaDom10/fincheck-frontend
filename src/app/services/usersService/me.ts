b47e2cb7fa15a5eacc9853f63ed3571c6ec18391
Rafael Domingues <r-domingues@outlook.com>
2024-01-11 21:12:04 -0300

    feat: add service for users

---
+++ src/app/services/usersService/me.ts
@@ -0,0 +1,11 @@
+import { httpClient } from "../HttpClient"
+
+interface MeResponse {
+  name: string
+  email: string
+}
+
+export async function me() {
+  const { data } = await httpClient.get<MeResponse>('/users/me')
+  return data
+}
