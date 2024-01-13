5cd81002aab8aded9a099f3750f976c456a20115
Rafael Domingues <r-domingues@outlook.com>
2024-01-13 18:13:54 -0300

    feat: add user menu component

---
+++ src/view/components/UserMenu.tsx
@@ -0,0 +1,9 @@
+export function UserMenu() {
+  return (
+    <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border-teal-100">
+      <span className="text-sm text-teal-900 tracking-[-0.5px] font-medium">
+        RD
+      </span>
+    </div>
+  )
+}
