cbdadbfbeaaefa01330c74dba631a1e983fcae76
Rafael Domingues <r-domingues@outlook.com>
2024-01-30 20:11:47 -0300

    refactor: implement helper text error component

--- src/view/components/Input.tsx
+++ src/view/components/Input.tsx
@@ -1,6 +1,6 @@
 import { type ComponentProps, forwardRef } from 'react'
-import { CrossCircledIcon } from '@radix-ui/react-icons'
 import { cn } from '../../app/utils/cs'
+import { HelperTextError } from './HelperTextError'
 
 interface InputProps extends ComponentProps<'input'> {
   name: string
@@ -12,39 +12,30 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     const inputId = id ?? name
 
     return (
-    <div className="relative">
-      <input
-        {...props}
-        ref={ref}
-        name={name}
-        id={inputId}
-        placeholder=" "
-        className={cn(
-          'bg-white w-full rounded-lg border px-3 border-gray-500 h-[52px] text-gray-800 pt-4 peer placeholder-shown:p-3 focus:border-gray-800 transition-all outline-none',
-          error && 'border-red-900 focus:border-red-900',
-          className
-        )}
-      />
-      <label
-        htmlFor={inputId}
-        className="absolute text-xs left-[13px] top-2 text-gray-700 pointer-events-none
+      <div className="relative">
+        <input
+          {...props}
+          ref={ref}
+          name={name}
+          id={inputId}
+          placeholder=" "
+          className={cn(
+            'bg-white w-full rounded-lg border px-3 border-gray-500 h-[52px] text-gray-800 pt-4 peer placeholder-shown:p-3 focus:border-gray-800 transition-all outline-none',
+            error && 'border-red-900 focus:border-red-900',
+            className
+          )}
+        />
+        <label
+          htmlFor={inputId}
+          className="absolute text-xs left-[13px] top-2 text-gray-700 pointer-events-none
           peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all"
-      >
-        {placeholder}
-      </label>
+        >
+          {placeholder}
+        </label>
 
-      {
-        error &&
-        (
-          <div className="flex gap-2 items-center mt-2 text-red-900">
-            <CrossCircledIcon />
-            <span className="text-xs">{error}</span>
-          </div>
-        )
+        {error && <HelperTextError error={error} />}
 
-      }
-
-    </div>
+      </div>
     )
   })
 Input.displayName = 'Input'
