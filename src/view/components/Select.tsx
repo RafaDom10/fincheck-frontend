1a38e62131f85d8d0897cae1b07f5f0f9b1f1e98
Rafael Domingues <r-domingues@outlook.com>
2024-01-30 20:12:01 -0300

    feat: add select component

--- src/view/components/Select.tsx
+++ src/view/components/Select.tsx
@@ -1,91 +1,84 @@
-import React from 'react'
 import * as RdxSelect from '@radix-ui/react-select'
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
 import { cn } from '../../app/utils/cs'
+import { HelperTextError } from './HelperTextError'
+import { useState } from 'react'
 
-const SelectItem = React.forwardRef(({ children, className, ...props }, forwardedRef) => {
+interface SelectProps {
+  className?: string
+  error?: string
+  placeholder?: string
+  options: Array<{
+    value: string
+    label: string
+  }>
+}
+
+export function Select ({ className, placeholder, error, options }: SelectProps) {
+  const [selectedValue, setSelectedValue] = useState('')
+
+  function handleSelected (value: string) {
+    setSelectedValue(value)
+  }
+
   return (
-    <RdxSelect.Item
-      className={cn(
-        'text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
-        className
-      )}
-      {...props}
-      ref={forwardedRef}
-    >
-      <RdxSelect.ItemText>{children}</RdxSelect.ItemText>
-      <RdxSelect.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-        <CheckIcon />
-      </RdxSelect.ItemIndicator>
-    </RdxSelect.Item>
-  )
-})
+    <div>
+      <div className='relative'>
+        <label className={cn(
+          'absolute z-10 top-1/2 -translate-y-1/2 left-3 text-gray-700 pointer-events-none',
+          selectedValue && 'text-xs left-[13px] top-2 transition-all translate-y-0'
+        )}>
+          {placeholder}
+        </label>
+        <RdxSelect.Root onValueChange={handleSelected}>
+          <RdxSelect.Trigger
+            className={cn(
+              'bg-white w-full rounded-lg border px-3 border-gray-500 h-[52px] text-gray-800 focus:border-gray-800 transition-all outline-none text-left relative pt-4',
+              error && 'border-red-900 focus:border-red-900',
+              className
+            )}
+          >
+            <RdxSelect.Value />
 
-export function Select () {
-  return (
-    <RdxSelect.Root>
-      <RdxSelect.Trigger
-        className={cn(
-          'bg-white w-full rounded-lg border px-3 border-gray-500 h-[52px] text-gray-800 pt-4 peer placeholder-shown:p-3 focus:border-gray-800 transition-all outline-none'
-          // error && 'border-red-900 focus:border-red-900',
-          // className
-        )}
-      >
-        <RdxSelect.Value placeholder="Select a fruit…" />
+            <RdxSelect.Icon className="absolute right-3 top-1/2 -translate-y-1/2">
+              <ChevronDownIcon className='w-6 h-6 text-gray-800' />
+            </RdxSelect.Icon>
+          </RdxSelect.Trigger>
+          <RdxSelect.Portal>
+            <RdxSelect.Content
+              className="z-[99] overflow-hidden bg-white rounded-2xl border border-gray-100 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]"
+            >
 
-        <RdxSelect.Icon className="text-violet11">
-          <ChevronDownIcon />
-        </RdxSelect.Icon>
-      </RdxSelect.Trigger>
-      <RdxSelect.Portal>
-        <RdxSelect.Content className="z-[99] overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-          <RdxSelect.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
-            <ChevronUpIcon />
-          </RdxSelect.ScrollUpButton>
-          <RdxSelect.Viewport className="p-[5px]">
-            <RdxSelect.Group>
-              <RdxSelect.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
-                Fruits
-              </RdxSelect.Label>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
-            </RdxSelect.Group>
+              <RdxSelect.ScrollUpButton
+                className="flex items-center justify-center h-[25px] bg-white text-gray-800 cursor-default"
+              >
+                <ChevronUpIcon />
+              </RdxSelect.ScrollUpButton>
 
-            <RdxSelect.Separator className="h-[1px] bg-violet6 m-[5px]" />
+              <RdxSelect.Viewport className="p-2">
+                {options.map(({ value, label }) => (
+                  <RdxSelect.Item
+                    key={value}
+                    value={value}
+                    className="p-2 text-gray-800 text-sm data-[state=checked]:font-bold outline-none data-[highlighted]:bg-gray-50 rounded-lg transition-colors"
+                  >
+                    <RdxSelect.ItemText>{label}</RdxSelect.ItemText>
+                  </RdxSelect.Item>
+                ))}
+              </RdxSelect.Viewport>
 
-            <RdxSelect.Group>
-              <RdxSelect.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
-                Vegetables
-              </RdxSelect.Label>
-              <SelectItem value="aubergine">Aubergine</SelectItem>
-              <SelectItem value="broccoli">Broccoli</SelectItem>
-              <SelectItem value="carrot" disabled>
-                Carrot
-              </SelectItem>
-              <SelectItem value="courgette">Courgette</SelectItem>
-              <SelectItem value="leek">Leek</SelectItem>
-            </RdxSelect.Group>
+              <RdxSelect.ScrollDownButton
+                className="flex items-center justify-center h-[25px] bg-white text-gray-800 cursor-default"
+              >
+                <ChevronDownIcon />
+              </RdxSelect.ScrollDownButton>
 
-            <RdxSelect.Separator className="h-[1px] bg-violet6 m-[5px]" />
+            </RdxSelect.Content>
+          </RdxSelect.Portal>
+        </RdxSelect.Root>
+      </div>
 
-            <RdxSelect.Group>
-              <RdxSelect.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
-                Meat
-              </RdxSelect.Label>
-              <SelectItem value="beef">Beef</SelectItem>
-              <SelectItem value="chicken">Chicken</SelectItem>
-              <SelectItem value="lamb">Lamb</SelectItem>
-              <SelectItem value="pork">Pork</SelectItem>
-            </RdxSelect.Group>
-          </RdxSelect.Viewport>
-          <RdxSelect.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
-            <ChevronDownIcon />
-          </RdxSelect.ScrollDownButton>
-        </RdxSelect.Content>
-      </RdxSelect.Portal>
-    </RdxSelect.Root>
+      {error && <HelperTextError error={error} />}
+    </div>
   )
 }
