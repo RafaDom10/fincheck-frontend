cd4467ddbd030986f5f7ab6a05640ed916b4dd05
Rafael Domingues <r-domingues@outlook.com>
2024-02-05 22:35:34 -0300

    feat: add zod schema

--- src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.tsx
+++ src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.tsx
@@ -1,4 +1,14 @@
+import { z } from 'zod'
 import { useDashboard } from '../../components/DashboardContext/useDashboard'
+
+const schema = z.object({
+  initialBalance: z.string().min(1, 'Saldo inicial é obrigatório'),
+  name: z.string().min(1, 'Nome da conta é obrigatório'),
+  type: z.enum([ 'CHECKING', 'INVESTMENT', 'CASH' ]),
+  color: z.string().min(1, 'Cor é obrigatória')
+})
+
+type FormData = z.infer<typeof schema>
 
 export function useNewAccountModalController () {
   const {
