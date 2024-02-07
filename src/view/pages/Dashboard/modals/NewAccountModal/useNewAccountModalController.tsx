0a662b44ae6b8399bc7973cabd2379ec77013427
Rafael Domingues <r-domingues@outlook.com>
2024-02-06 22:32:14 -0300

    refactor: implements account handleSubmit to create new bank account

--- src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.tsx
+++ src/view/pages/Dashboard/modals/NewAccountModal/useNewAccountModalController.tsx
@@ -1,10 +1,16 @@
 import { z } from 'zod'
 import { useDashboard } from '../../components/DashboardContext/useDashboard'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { bankAccountService } from '../../../../../app/services/bankAccountService'
+import toast from 'react-hot-toast'
+import { currencyStringToNumber } from '../../../../../app/utils/currencyStringToNumber'
 
 const schema = z.object({
   initialBalance: z.string().min(1, 'Saldo inicial é obrigatório'),
   name: z.string().min(1, 'Nome da conta é obrigatório'),
-  type: z.enum([ 'CHECKING', 'INVESTMENT', 'CASH' ]),
+  type: z.enum(['CHECKING', 'INVESTMENT', 'CASH']),
   color: z.string().min(1, 'Cor é obrigatória')
 })
 
@@ -16,8 +22,41 @@ export function useNewAccountModalController () {
     closeNewAccountModal
   } = useDashboard()
 
+  const {
+    register,
+    handleSubmit: hookFormHandleSubmit,
+    formState: { errors },
+    control,
+    reset
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  })
+
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: bankAccountService.create
+  })
+
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    try {
+      await mutateAsync({
+        ...data,
+        initialBalance: currencyStringToNumber(data.initialBalance)
+      })
+      toast.success('Conta cadastrada com sucesso!')
+      closeNewAccountModal()
+      reset()
+    } catch {
+      toast.error('Erro ao cadastrar conta.')
+    }
+  })
+
   return {
     isNewAccountModalOpen,
-    closeNewAccountModal
+    closeNewAccountModal,
+    register,
+    errors,
+    handleSubmit,
+    control,
+    isPending
   }
 }
