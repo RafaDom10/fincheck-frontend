73434a5748633bbda71cfd3b24bd94ab6eca3c48
Rafael Domingues <r-domingues@outlook.com>
2024-02-08 20:30:09 -0300

    feat: add edit account modal on dash

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import { Accounts } from './components/Accounts'
 import { DashboardProvider } from './components/DashboardContext'
 import { Fab } from './components/Fab'
 import { Transactions } from './components/Transactions'
+import { EditAccountModal } from './modals/EditAccountModal'
 import { NewAccountModal } from './modals/NewAccountModal'
 import { NewTransactionsModal } from './modals/NewTransactionsModal'
 
@@ -26,6 +27,7 @@ export function Dashboard () {
 
         <Fab />
         <NewAccountModal />
+        <EditAccountModal />
         <NewTransactionsModal />
       </div>
     </DashboardProvider>
