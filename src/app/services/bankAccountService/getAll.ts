06ce455f9f1d41a98ce147d5245176f0f545199e
Rafael Domingues <r-domingues@outlook.com>
2024-02-08 20:29:41 -0300

    refactor: change type response

--- src/app/services/bankAccountService/getAll.ts
+++ src/app/services/bankAccountService/getAll.ts
@@ -1,14 +1,7 @@
+import { BankAccount } from '../../entities/BankAccount'
 import { httpClient } from '../HttpClient'
 
-type BankAccountResponse = Array<{
-  id: string
-  userId: string
-  name: string
-  initialBalance: number
-  type: 'INVESTMENT' | 'CHECKING' | 'CASH'
-  color: string
-  currentBalance: number
-}>
+type BankAccountResponse = Array<BankAccount>
 
 export async function getAll () {
   const { data } = await httpClient.get<BankAccountResponse>('/bank-accounts')
