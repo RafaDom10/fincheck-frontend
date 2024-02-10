31cb76d7de8e07e750a0b2eee9f8cfaf088a8100
Rafael Domingues <r-domingues@outlook.com>
2024-02-09 21:13:26 -0300

    feat: add consumer

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,35 +1,39 @@
 import { Logo } from '../../components/Logo'
 import { UserMenu } from '../../components/UserMenu'
 import { Accounts } from './components/Accounts'
-import { DashboardProvider } from './components/DashboardContext'
+import { DashboardContext, DashboardProvider } from './components/DashboardContext'
 import { Fab } from './components/Fab'
 import { Transactions } from './components/Transactions'
 import { EditAccountModal } from './modals/EditAccountModal'
 import { NewAccountModal } from './modals/NewAccountModal'
 import { NewTransactionsModal } from './modals/NewTransactionsModal'
 
-export function Dashboard () {
+export function Dashboard() {
   return (
     <DashboardProvider>
-      <div className="h-full w-full p-4 md:p-8 md:pt-6 flex flex-col gap-4">
-        <header className="h-12 flex flex-row items-center justify-between">
-          <Logo className="h-6 text-teal-900" />
-          <UserMenu />
-        </header>
-        <main className="flex-1 flex flex-col md:flex-row gap-4 max-h-full">
-          <div className="md:w-1/2">
-            <Accounts />
-          </div>
-          <div className="md:w-1/2">
-            <Transactions />
-          </div>
-        </main>
+      <DashboardContext.Consumer>
+        {({ accountBeingEdited }) => (
+          <div className="h-full w-full p-4 md:p-8 md:pt-6 flex flex-col gap-4">
+            <header className="h-12 flex flex-row items-center justify-between">
+              <Logo className="h-6 text-teal-900" />
+              <UserMenu />
+            </header>
+            <main className="flex-1 flex flex-col md:flex-row gap-4 max-h-full">
+              <div className="md:w-1/2">
+                <Accounts />
+              </div>
+              <div className="md:w-1/2">
+                <Transactions />
+              </div>
+            </main>
 
-        <Fab />
-        <NewAccountModal />
-        <EditAccountModal />
-        <NewTransactionsModal />
-      </div>
+            <Fab />
+            <NewAccountModal />
+            <NewTransactionsModal />
+            {accountBeingEdited && <EditAccountModal />}
+          </div>
+        )}
+      </DashboardContext.Consumer>
     </DashboardProvider>
   )
 }
