5e43f834575ab7e21fcbca5338d853f8a89eb015
Rafael Domingues <r-domingues@outlook.com>
2024-02-09 21:11:23 -0300

    feat: add update service method

---
+++ src/app/services/bankAccountService/update.ts
@@ -0,0 +1,14 @@
+import { httpClient } from '../HttpClient'
+
+export interface UpdateBankAccountParams {
+  id: string
+  name: string
+  initialBalance: number
+  color: string
+  type: 'CHECKING' | 'INVESTMENT' | 'CASH'
+}
+
+export async function update ({ id, ...params }: UpdateBankAccountParams) {
+  const { data } = await httpClient.put(`/bank-accounts/${id}`, params)
+  return data
+}
