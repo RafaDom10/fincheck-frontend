93660e22f8d9a5d9499870f01d665bc43fadae8a
Rafael Domingues <r-domingues@outlook.com>
2024-02-12 19:28:22 -0300

    feat: finish delete modal

--- src/view/components/ConfirmDeleteModal.tsx
+++ src/view/components/ConfirmDeleteModal.tsx
@@ -1,13 +1,32 @@
+import { Button } from "./Button";
 import { Modal } from "./Modal";
+import { TrashIcon } from "./icons/TrashIcon";
 
 interface ConfirmDeleteModalProps {
   onClose(): void
+  onConfirm(): void
+  title: string
+  description?: string
 }
 
-export function ConfirmDeleteModal ({ onClose }: ConfirmDeleteModalProps ) {
+export function ConfirmDeleteModal({ onClose, onConfirm, title, description }: ConfirmDeleteModalProps) {
   return (
     <Modal open={true} title="Excluir" onClose={onClose}>
-      Content
+      <div className="flex flex-col items-center text-center gap-6">
+        <div className="w-[52px] h-[52px] rounded-full bg-red-50 flex items-center justify-center">
+          <TrashIcon className="text-red-900 w-6 h-6" />
+        </div>
+        <p className="w-[180px] text-gray-800 tracking-[-0.5px] font-bold">{title}</p>
+        {description && <p className="tracking-[-0.5px] text-gray-800">{description}</p>}
+      </div>
+      <div className="mt-10 space-y-4">
+        <Button className="w-full" variant='danger' onClick={onConfirm}>
+          Sim, desejo excluir
+        </Button>
+        <Button className="w-full" variant="ghost" onClick={onClose}>
+          Cancelar
+        </Button>
+      </div>
     </Modal>
   )
 }
