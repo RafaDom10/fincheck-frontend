d2c73e29f4ea4a684211aeb8df3c0717efe2d11e
Rafael Domingues <r-domingues@outlook.com>
2024-02-17 01:37:29 -0300

    feat: add initial letters of username

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -3,14 +3,14 @@ import { DropdownMenu } from "./DropdownMenu"
 import { useAuth } from "../../app/hooks/useAuth"
 
 export function UserMenu() {
-  const { signout } = useAuth()
+  const { signout, user } = useAuth()
 
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border-teal-100">
+        <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border-teal-100" role="button">
           <span className="text-sm text-teal-900 tracking-[-0.5px] font-medium">
-            RD
+            {user?.name.slice(0,2).toUpperCase()}
           </span>
         </div>
       </DropdownMenu.Trigger>
